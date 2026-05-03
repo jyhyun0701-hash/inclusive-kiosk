2ff447a910309af69f283f91a4905446e0dd9449
hyeonjiyeon <jyhyun0701@gmail.com>
2026-05-04 00:03:15 +0900

    - delete not use file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import KeypadPage from './pages/KeypadPage';
-import './index.css';
 
 const isKeypad = window.location.pathname === '/keypad';
 
