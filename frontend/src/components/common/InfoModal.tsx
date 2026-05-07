181c649711665cc5864b1488fb785814fd1b36b4
hyeonjiyeon <jyhyun0701@gmail.com>
2026-05-07 21:28:50 +0900

    - add - infoModal images, css

--- frontend/src/components/common/InfoModal.tsx
+++ frontend/src/components/common/InfoModal.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import imgKeypadInfo from '../../assets/images/keypad-info.png';
+import imgPopupInfo from  '../../assets/images/popup-info.png';
 
 interface InfoModalProps {
   onClose: () => void;
@@ -15,33 +17,25 @@ const InfoModal: React.FC<InfoModalProps> = ({ onClose }) => {
 
   return (
     <div className="modal-overlay" role="dialog" aria-modal="true" aria-label="이용 안내">
-      <div className="info-modal">
-        <p style={{ fontWeight: 700, marginBottom: 12, fontSize: '1.1rem' }}>
-          키패드 사용법 안내입니다.
-        </p>
-        <p style={{ color: '#555', lineHeight: 1.9 }}>
-          상축, 하축 버튼은 항목 이동시에 사용하며,<br />
+      <div className="info-modal content-verify" style={{ justifyContent: 'flex-start' }}>
+        <div className="info-title-box" style={{ margin: '-3px -50px', borderRadius: 'var(--r) var(--r) 0 0' }}>
+          <img src={imgPopupInfo} alt="imgPopupInfo" />
+          <p className="info-title">키패드 사용법 안내 팝업창</p>
+        </div>
+
+        <p>
+          키오스크 하단에 위치한 음성 켜기 버튼 입력 시, 키패드 기능과 음성 안내 기능이 활성화됩니다.
+          키패드의 상측, 하측 버튼은 항목 이동시에 사용하며,
           원형 버튼은 입력시 사용합니다.
         </p>
 
-        <div className="info-keypad">
-          {/* 위 */}
-          <div />
-          <div className="info-key">∧</div>
-          <div />
-          {/* 가운데 */}
-          <div className="info-key">&#60;</div>
-          <div className="info-key" style={{ background: '#aaa' }}>○</div>
-          <div className="info-key">&#62;</div>
-          {/* 아래 */}
-          <div />
-          <div className="info-key">∨</div>
-          <div />
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <img src={imgKeypadInfo} alt="imgKeypadInfo" style={{ width: '300px', height: '300px', objectFit: 'contain' }} />
         </div>
 
         <button
           className="modal-btn confirm"
-          style={{ width: '100%', marginTop: 8 }}
+          style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '350px' }}
           onClick={onClose}
           autoFocus
         >
